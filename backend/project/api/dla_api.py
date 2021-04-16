d1d403a9e9cd2399d02f1169a1c100a472fc26b4
Ubuntu <magicapqa@gmail.com>
2021-04-16 08:43:31 +0000

    changes for memory

--- backend/project/api/dla_api.py
+++ backend/project/api/dla_api.py
@@ -2,10 +2,13 @@ import cv2
 import numpy as np
 from flask import Blueprint, jsonify, request
 
-from project.predictor import make_predictions
+from project.predictor import make_predictions, prepare_predictor
 
 dla_blueprint = Blueprint("", __name__)
 
+PREDICTOR = None
+if not PREDICTOR:
+    PREDICTOR = prepare_predictor()
 
 @dla_blueprint.route("/analyse-image-json", methods=["POST"])
 def analyse_image_json():
@@ -16,7 +19,6 @@ def analyse_image_json():
     nparr = np.frombuffer(img_str, np.uint8)
 
     image = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)  # BGR format
-
     print(f"Image shape: {image.shape}")
 
     if len(image.shape) == 2:
@@ -24,6 +26,10 @@ def analyse_image_json():
     elif image.shape[2] == 4:
         image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
 
-    json_data = make_predictions(image, True)
+    global PREDICTOR
 
+    json_data = make_predictions(image, True,PREDICTOR )
+    del nparr
+    del image
+    del img_str
     return jsonify(json_data)
