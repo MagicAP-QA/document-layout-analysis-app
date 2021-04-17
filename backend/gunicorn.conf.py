7627537a8268629e68803d6c3a17daf80009f042
zafar <zafar>
2021-04-17 14:00:43 +0000

    changed config

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -2,5 +2,6 @@ import multiprocessing
 
 bind = "0.0.0.0:5000"
 # workers = multiprocessing.cpu_count() * 2 + 1
-workers = multiprocessing.cpu_count()
-timeout = "60"
+timeout = 1200
+threads = 1
+workers = 1
